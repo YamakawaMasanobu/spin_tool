049c5439d204b0c1f9e2c2db3cc5ec38bbcf3a9f
山川眞延 <>
2021-08-30 13:45:50 +0900

    シミュレーション実行するファイルを指定可能に変更

--- getstdout.py
+++ getstdout.py
@@ -1,10 +1,12 @@
 import subprocess, sys
 
-cp = subprocess.run(['spin', '-p', 'Katsupay_spin.pml'], encoding='utf-8', stdout=subprocess.PIPE)
+filename = input("file name: ")
+
+cp = subprocess.run(['spin', '-p', filename], encoding='utf-8', stdout=subprocess.PIPE)
 print(f'*** file names: \n{cp.stdout}'
       f'*** total {len(cp.stdout.splitlines())} files')
 with open('result_simu.txt', 'w') as fp:
-    cp = subprocess.run(['spin', '-p', 'Katsupay_spin.pml'], stdout=fp)
+    cp = subprocess.run(['spin', '-p', filename], stdout=fp)
 
 args = ["python3", "toolish.py"]
 try:
