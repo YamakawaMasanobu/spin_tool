1e04f8d2f980f9fc336edda5b966032b0662f31c
YamakawaMasanobu <hcu_yama@ca.info.hiroshima-cu.ac.jp>
2021-10-06 07:28:01 +0900

    状態遷移図の表示を可能に

--- toolish.py
+++ toolish.py
@@ -13,11 +13,16 @@ rootnode_xpos = 0 #ルートノードのx座標を保持する変数
 node_pos_list = []
 num_proc = 0 #生成したプロセス数
 current_step = 0
+current_state_list= []
+state_link_list = []
+proc_state_list = []
 
 fw = open("sequence.dot", "w")
 f = open("result_simu.txt", "r")
+sm = open("statemachine.pu", "w")
 
 fw.write("digraph sequence{\n")
+sm.write("@startuml\n\n")
 
 line = f.readline()
 
@@ -41,6 +46,8 @@ while(line):    #ログを一行ずつ解析
         proc = line[mo_start:mo_start+7]
         start_node_list += [proc]
         node_pos_list += [[rootnode_xpos, 0]]
+        proc_state_list += [["*"]]
+        current_state_list += ["[*]"]
         fw.write(str(node_id) + "[label = \"" + proc + "\",\n" )
         fw.write("pos = \"" + str(rootnode_xpos) + ",0!\"];\n")
         current_id_list += [node_id]
@@ -51,9 +58,11 @@ while(line):    #ログを一行ずつ解析
     else:
         step_num = re.match(r"\s*[0-9]+", line)
         index_mo = re.search(r'[p][r][o][c]\s+[0-9]', line)
+        state_mo = re.search(r'[s][t][a][t][e]\s[0-9]+', line)
         if index_mo != None:
             mo_start = index_mo.start()
-            proc_comp = line[mo_start:mo_start+7]
+            mo_end = index_mo.end()
+            proc_comp = line[mo_start:mo_end]
             if proc_comp in start_node_list:
                 snl_index = start_node_list.index(proc_comp)
                 if current_step != step_num.group():
@@ -66,11 +75,31 @@ while(line):    #ログを一行ずつ解析
                 # node_label = node_id -2
                 fw.write(str(node_id) + "[label = \"" + str(step_num.group()) + "\",\n")
                 fw.write("pos = \"" + str(node_pos_list[snl_index][0]) + "," + str(node_pos_list[snl_index][1]) + "!\"];\n")
-                fw.write(str(current_id_list[snl_index]) + "->" + str(node_id) + ";\n")
+                fw.write(str(current_id_list[snl_index]) + "->" + str(node_id) + "\n")
+                edge_label_mo = re.search(r"[\[].*[\]]",line)
+                # edge_label = edge_label_mo.group().replace("[", "")
+                # edge_label = edge_label.replace("]", "")
+                edge_label = edge_label_mo.group()
+                fw.write("[label = \"" + edge_label + "\"]\n")
                 current_id_list[snl_index] = node_id
                 node_id += 1
                 seq_num += 1
                 current_step = step_num.group()
+                
+            smo_start = state_mo.start()
+            smo_end = state_mo.end()
+            state = line[smo_start:smo_end] + "_" + str(snl_index)
+            nonspace_state = state.replace(" ", "")
+            state_link = current_state_list[snl_index] + "-->" + nonspace_state
+            if state_link not in state_link_list:
+                sm.write(state_link + "\n")
+                state_link_list += [state_link]
+                proc_state_list[snl_index] += [state_link]
+            else:
+                pass
+            current_state_list[snl_index] = nonspace_state
+            
+            
         elif index_mo == None:
             break
 
@@ -78,9 +107,13 @@ while(line):    #ログを一行ずつ解析
     line = f.readline()
 
 fw.write("}")
+sm.write("@enduml")
 f.close()
 fw.close()
+sm.close
 print(start_node_list)
 print(current_id_list)
 print(node_pos_list)
+# print(state_link_list)
 print(num_proc)
+# print(proc_state_list)
