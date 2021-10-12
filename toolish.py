8c69bffb3e4fd3e205cf5fe23e121758a55f2927
YamakawaMasanobu <hcu_yama@ca.info.hiroshima-cu.ac.jp>
2021-10-12 14:09:07 +0900

    基本ブロックに対応した状態遷移図を出力する（未完成の試作段階）

--- toolish.py
+++ toolish.py
@@ -2,6 +2,7 @@ from platform import node
 from graphviz import Graph
 from graphviz import Digraph
 import re
+import collections
 
 start_node_list = [] #開始ノードを格納するためのリスト
 current_id_list = [] #intlist
@@ -15,7 +16,9 @@ num_proc = 0 #生成したプロセス数
 current_step = 0
 current_state_list= []
 state_link_list = []
-proc_state_list = []
+state_message_list = []
+childnode_list = []
+parentnode_list = [] 
 
 fw = open("sequence.dot", "w")
 f = open("result_simu.txt", "r")
@@ -25,6 +28,50 @@ fw.write("digraph sequence{\n")
 sm.write("@startuml\n\n")
 
 line = f.readline()
+
+def write_statemachine(parentnode_list, state_in_list, state_out_list, num_proc,state_link_list):
+    i = 0
+    roop_counter = 0
+    state_num = 1
+    while i < num_proc:
+        j = 0
+        while j < len(parentnode_list[i]):
+            if state_in_list[i][parentnode_list[i][j]] == 1 and state_out_list[i][parentnode_list[i][j]]:
+                if roop_counter == 0:
+                    # sm.write("state state_" + str(state_num) + "{\n")
+                    # sm.write("state " + parentnode_list[i][j] + "{\n}\n")
+                    state_num += 1
+                    roop_counter += 1
+                else:
+                    # sm.write("state " + parentnode_list[i][j] + "{\n}\n")
+                    roop_counter = 0
+            else:              
+                # sm.write(state_link_list[i][j] + "\n")
+                pass
+            j += 1
+        i += 1
+    # i = 0
+# while i < num_proc:
+#     j = 0
+#     while j < len(state_link_list[i]):
+#         print(num_in_out[0][str(state_link_list[i][j])])
+#         j+=1
+#         # if num_in_out[0][str(state_link_list[i][j])]:
+
+#     i += 1
+
+def count_in_out (childnode_list, num_proc, parentnode_list, state_link_list):
+    i = 0
+    state_out_list = []
+    state_in_list = []
+    while i < num_proc:
+        state_out_list += [collections.Counter(childnode_list[i])]
+        state_in_list += [collections.Counter(parentnode_list[i])]
+        i += 1
+    # print(state_in_list[0][parentnode_list[0][2]])
+    write_statemachine(parentnode_list, state_in_list, state_out_list, num_proc, state_link_list)
+    # return [state_in_list, state_out_list]
+
 
 while(line):    #ログを一行ずつ解析
     start = line.find("Starting")
@@ -46,8 +93,11 @@ while(line):    #ログを一行ずつ解析
         proc = line[mo_start:mo_start+7]
         start_node_list += [proc]
         node_pos_list += [[rootnode_xpos, 0]]
-        proc_state_list += [["*"]]
+        childnode_list += [[]]
         current_state_list += ["[*]"]
+        parentnode_list += [[]]
+        state_link_list += [[]]
+        state_message_list += [[]]
         fw.write(str(node_id) + "[label = \"" + proc + "\",\n" )
         fw.write("pos = \"" + str(rootnode_xpos) + ",0!\"];\n")
         current_id_list += [node_id]
@@ -60,6 +110,7 @@ while(line):    #ログを一行ずつ解析
         index_mo = re.search(r'[p][r][o][c]\s+[0-9]', line)
         state_mo = re.search(r'[s][t][a][t][e]\s[0-9]+', line)
         if index_mo != None:
+            #以下シーケンス図に関する記述
             mo_start = index_mo.start()
             mo_end = index_mo.end()
             proc_comp = line[mo_start:mo_end]
@@ -85,16 +136,21 @@ while(line):    #ログを一行ずつ解析
                 node_id += 1
                 seq_num += 1
                 current_step = step_num.group()
-                
-            smo_start = state_mo.start()
+
+            #以下状態線図に関する記述    
+            smo_start = state_mo.start()  #状態遷移をリスト化
             smo_end = state_mo.end()
             state = line[smo_start:smo_end] + "_" + str(snl_index)
             nonspace_state = state.replace(" ", "")
             state_link = current_state_list[snl_index] + "-->" + nonspace_state
-            if state_link not in state_link_list:
+            if state_link not in state_link_list[snl_index]:
                 sm.write(state_link + "\n")
-                state_link_list += [state_link]
-                proc_state_list[snl_index] += [state_link]
+                sm.write(nonspace_state + ":" + edge_label + "\n")
+                state_message = nonspace_state + ":" + edge_label + "\n"
+                state_link_list[snl_index] += [state_link]
+                state_message_list[snl_index] += [state_message]
+                childnode_list[snl_index] += [nonspace_state]
+                parentnode_list[snl_index] += [current_state_list[snl_index]]
             else:
                 pass
             current_state_list[snl_index] = nonspace_state
@@ -105,15 +161,34 @@ while(line):    #ログを一行ずつ解析
 
 
     line = f.readline()
+num_in_out = count_in_out(childnode_list, num_proc, parentnode_list, state_link_list)
+# i = 0
+# while i < num_proc:
+#     j = 0
+#     while j < len(state_link_list[i]):
+#         print(num_in_out[0][str(state_link_list[i][j])])
+#         j+=1
+#         # if num_in_out[0][str(state_link_list[i][j])]:
+
+#     i += 1
+
+
+
+
 
 fw.write("}")
 sm.write("@enduml")
 f.close()
 fw.close()
 sm.close
-print(start_node_list)
-print(current_id_list)
-print(node_pos_list)
-# print(state_link_list)
-print(num_proc)
-# print(proc_state_list)
+# print(start_node_list)
+# print(current_id_list)
+# print(node_pos_list)
+print(state_link_list)
+print("\n\n\n")
+print(state_message_list)
+# print(num_proc)
+# print("\n\n\n")
+# print(childnode_list)
+# print("\n\n\n")
+# print(parentnode_list)
