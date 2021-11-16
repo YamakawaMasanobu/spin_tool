c7b0dac9ad6b4726e1e1a15d4350883ae031fe1d
YamakawaMasanobu <hcu_yama@ca.info.hiroshima-cu.ac.jp>
2021-11-16 15:57:09 +0900

    ログファイルの内容をdファイルの情報をもとに書き換えて再出力

--- analyze_result_simu.py
+++ analyze_result_simu.py
@@ -1,9 +1,11 @@
-#ランダムシミュレーションのログとSPINSPIDERのdotファイルから抽出された情報をてらしあわせる
+#ランダムシミュレーションのログをdファイルをもとに編集
+#メッセージの書き換えと不要な行の削除
 
 import re
 import analyze_dfile
 
 f = open("result_simu.txt", "r")
+fw = open ("edited_result_simu.txt", "w")
 f_line = f.readline()
 line_num = 0
 
@@ -11,23 +13,21 @@ while(f_line):
     creates = f_line.find("creates")
     proc_log_mo = re.match(r"\s*[0-9]+:\s+proc", f_line)
     breakpoint_mo = re.match(r"#process", f_line)
-    if proc_log_mo != None and creates == -1:
-        tmp_list =f_line.split()
-        # print(tmp_list)
-        proc_name_mo = re.search(r"(\()(.*)(:)(.*)", tmp_list[3])
+    if creates != -1:
+        fw.write(f_line)
+    elif proc_log_mo != None and creates == -1:
+        proc_name_mo = re.search(r"(\()(.*)(:)([0-9]+)(\))", f_line)
         proc_name = proc_name_mo.group(2)
-        line_mo = re.search(r"(.*)(\.pml:)([0-9]+)", tmp_list[4])
-        line_num = line_mo.group(3)
+        line_mo = re.search(r"(\.pml:)([0-9]+)", f_line)
+        line_num = line_mo.group(2)
         proc_key = analyze_dfile.proc_dict[proc_name].keys()
-        # print(proc_key)
         if line_num in proc_key:
-            new_line = f_line.replace(tmp_list[7], analyze_dfile.proc_dict[proc_name][line_num])
-            print(new_line) 
-            # print(line_num)
+            lc_mo = re.search(r"\[.*\]", f_line)
+            location_counter = lc_mo.group()
+            new_line = f_line.replace(location_counter, analyze_dfile.proc_dict[proc_name][line_num])
+            fw.write(new_line)
         else:
-            new_line.replace(f_line, "")
-            print(new_line)
-            # print(line_num)
+            pass
     elif breakpoint_mo != None:
         break
     else:
@@ -35,3 +35,4 @@ while(f_line):
 
     f_line = f.readline()
 f.close()
+fw.close()
