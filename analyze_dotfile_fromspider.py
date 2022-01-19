4eaa1f6a6ee28281f04254b1f997238eb50e709d
YamakawaMasanobu <hcu_yama@ca.info.hiroshima-cu.ac.jp>
2022-01-19 17:06:37 +0900

    SPINPIDERから得られるdotファイルを解析するプログラム

--- analyze_dotfile_fromspider.py
+++ analyze_dotfile_fromspider.py
@@ -3,9 +3,13 @@
 import re
 state_info_list = [] #状態の情報を格納するlistのlist
 edge_list=[]    #状態の接続関係を格納するstrのlist
+state_dict = {}
+sedge_list = []
+pstate_dict = {}
+cstate_dict = {}
 
-dotfilename = input("filename:")
-# dotfilename = "jikken.dot"
+# dotfilename = input("filename:")
+dotfilename = "jikken.dot"
 f = open(dotfilename, "r")
 line = f.readline()
 while(line):
@@ -21,7 +25,45 @@ while(line):
         pass
     line = f.readline()
 
-print(state_info_list)
-print(edge_list)
+i = 0
+temp_dict = {}
+while(i < len(state_info_list)):
+    j = 0
+    while(j < len(state_info_list[i]) - 1):
+        temp_dict[j] = state_info_list[i][j]
+        j += 1
+    state_dict[i] = temp_dict
+    i += 1
+
+
+i = 0
+while(i < len(edge_list)):
+    sedge_list = edge_list[i].split()
+    #親ノードの辞書を作成
+    if sedge_list[0] not in cstate_dict.keys():
+        cstate_dict[sedge_list[0]] = [sedge_list[2]]
+    else:
+        if sedge_list[2] not in cstate_dict[sedge_list[0]]:
+            cstate_dict[sedge_list[0]].append(sedge_list[2])
+        else:
+            pass
+    #子ノードの辞書を作成    
+    if sedge_list[2] not in pstate_dict.keys():
+        pstate_dict[sedge_list[2]] = [sedge_list[0]]
+    else:
+        if sedge_list[0] not in pstate_dict[sedge_list[2]]:
+            pstate_dict[sedge_list[2]].append(sedge_list[0])
+        else:
+            pass
+    
+    i += 1
+
+
+# print(pstate_dict)
+# print("\n\n")
+# print(cstate_dict)
+# print(state_dict)
+# print(edge_list)
+# print(state_info_list)
 
 f.close()
