6128f0bac372cca6376dcd8e18191450eb97cc43
YamakawaMasanobu <hcu_yama@ca.info.hiroshima-cu.ac.jp>
2022-01-19 17:05:58 +0900

    analyze_chkfile.pyとedit_result_simu.py両方で扱えるように変更

--- analyze_dfile.py
+++ analyze_dfile.py
@@ -1,11 +1,10 @@
 import re
-import collections
+from collections import defaultdict
 
-location_counter = ""
-line_num = 0
-state_num = 0
 process_name = ""
-proc_dict = {}
+proc_dict_forers = {}
+proc_dict_forchk= {}
+proc_num = -1
 
 f = open("dfile.txt", "r")
 line = f.readline()
@@ -15,15 +14,31 @@ while(line):
     state_mo = re.match(r"\s*(state)\s*[0-9]+", line)
 
     if proc_mo != None:
+        proc_num += 1
         Match_Object = re.match(r"(proctype)\s+(.*)", line)
         process_name = Match_Object.group(2)
-        proc_dict[process_name] = {}
+        proc_dict_forchk[proc_num] = {}
+        proc_dict_forers[process_name] = {}
     elif state_mo != None:
+        source_mo = re.search(r"(\s*)(state)(\s+)([1-9]+)(\s-\(tr)(.*)", line) #source state
         l_lc_mo = re.search(r"(.pml:)([0-9]+)\s(=>)\s(.*)", line) #ロケーションカウンターとその行番号を抽出
-        proc_dict[process_name][l_lc_mo.group(2)] = l_lc_mo.group(4)
+        if source_mo.group(4) not in proc_dict_forchk[proc_num]:
+            proc_dict_forchk[proc_num][source_mo.group(4)] = {} 
+        else:
+            pass
+        
+        if l_lc_mo.group(2) in proc_dict_forchk[proc_num][source_mo.group(4)]:
+            proc_dict_forers[process_name][l_lc_mo.group(2)].append(l_lc_mo.group(4))
+            proc_dict_forchk[proc_num][source_mo.group(4)].append(l_lc_mo.group(2) + ". " + l_lc_mo.group(4))       
+        else:
+            proc_dict_forers[process_name][l_lc_mo.group(2)] = [l_lc_mo.group(4)]
+            proc_dict_forchk[proc_num][source_mo.group(4)] = [l_lc_mo.group(2) + ". " + l_lc_mo.group(4)]
+        
     else:
         pass
     line = f.readline()
 
-print(proc_dict)
+print(proc_dict_forchk)
+print("\n\n")
+print(proc_dict_forers)
 f.close()
