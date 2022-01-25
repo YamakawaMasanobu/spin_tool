117853d00ccae4c17bb26e9b524efb14ee0c14a9
YamakawaMasanobu <hcu_yama@ca.info.hiroshima-cu.ac.jp>
2022-01-25 18:08:35 +0900

    最新バージョン

--- getstdout.py
+++ getstdout.py
@@ -8,8 +8,18 @@ print(f'*** file names: \n{cp.stdout}'
 with open('result_simu.txt', 'w') as fp:
     cp = subprocess.run(['spin', '-p', filename], stdout=fp)
 
-args = ["python3", "toolish.py"]
+cp = subprocess.run(['spin', '-a', filename], encoding='utf-8', stdout=subprocess.PIPE)
+cp = subprocess.run(['gcc', '-o', 'pan', 'pan.c'], encoding = 'utf-8', stdout=subprocess.PIPE)
+cp = subprocess.run(['./pan', '-d'], encoding='utf-8', stdout=subprocess.PIPE)
+print(f'*** file names: \n{cp.stdout}'
+      f'*** total {len(cp.stdout.splitlines())} files')
+with open('dfile.txt', 'w') as fp:
+    cp = subprocess.run(['./pan', '-d'], stdout=fp)
+
+args = ["python3", "UMLgenerater.py"]
 try:
     res = subprocess.check_output(args)
 except:
     print("Error")
+
+
