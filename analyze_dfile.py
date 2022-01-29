69054fe144bedbca844df046ef92ba2faeef6dc2
YamakawaMasanobu <hcu_yama@ca.info.hiroshima-cu.ac.jp>
2022-01-29 11:12:47 +0900

    状態名が同の異なったプログラムカウンタがリストに格納できていないバグを解消

--- analyze_dfile.py
+++ analyze_dfile.py
@@ -23,22 +23,30 @@ while(line):
         source_mo = re.search(r"(\s*)(state)(\s+)([0-9][0-9]*)(\s-\(tr)(.*)", line) #source state
         l_lc_mo = re.search(r"(.pml:)([0-9][0-9]*)\s(=>)\s(.*)", line) #ロケーションカウンターとその行番号を抽出
         if source_mo.group(4) not in proc_dict_forchk[proc_num]:
-            proc_dict_forchk[proc_num][source_mo.group(4)] = {} 
+            proc_dict_forchk[proc_num][source_mo.group(4)] = []
+        else:
+            pass
+            
+        if l_lc_mo.group(2) + "." + l_lc_mo.group(4) not in proc_dict_forchk[proc_num][source_mo.group(4)]:
+            proc_dict_forchk[proc_num][source_mo.group(4)].append(l_lc_mo.group(2) + "." + l_lc_mo.group(4))
         else:
             pass
         
-        if l_lc_mo.group(2) in proc_dict_forchk[proc_num][source_mo.group(4)]:
-            proc_dict_forers[process_name][l_lc_mo.group(2)].append(l_lc_mo.group(4))
-            proc_dict_forchk[proc_num][source_mo.group(4)].append(l_lc_mo.group(2) + ". " + l_lc_mo.group(4))       
+        if l_lc_mo.group(2) not in proc_dict_forers[process_name]:
+            proc_dict_forers[process_name][l_lc_mo.group(2)] = []
         else:
-            proc_dict_forers[process_name][l_lc_mo.group(2)] = [l_lc_mo.group(4)]
-            proc_dict_forchk[proc_num][source_mo.group(4)] = [l_lc_mo.group(2) + ". " + l_lc_mo.group(4)]
+            pass
+                        
+        if l_lc_mo.group(4) not in proc_dict_forers[process_name][l_lc_mo.group(2)]:
+            proc_dict_forers[process_name][l_lc_mo.group(2)].append(l_lc_mo.group(4))
+        else:
+            pass
         
     else:
         pass
     line = f.readline()
 
-print(proc_dict_forchk)
-print("\n\n")
-print(proc_dict_forers)
+# print(proc_dict_forchk)
+# print("\n\n")
+# print(proc_dict_forers)
 f.close()
